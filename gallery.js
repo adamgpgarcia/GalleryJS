4e3a31dd9f673d69a2ec21c38a1db4f6fb2f3942
Unknown <agarcia318@mail.csuchico.edu>
2017-04-25 12:11:27 -0700

    Finished

--- gallery.js
+++ gallery.js
@@ -1,6 +1,15 @@
+var chico = ["Bear", "Horse", "Riley's", "Frankies", "TacoBell"];
+var current;
 
+function shuffleGallery(){
 
-function shuffleGallery()
-{
+  var i = Math.floor(Math.random() * 5);
 
+  if(current == i){
+    while (current == i){
+      var i = Math.floor(Math.random() * 5);
+    }
+  }
+  document.getElementById("demo").innerHTML = chico[i];
+  current = i;
 }
