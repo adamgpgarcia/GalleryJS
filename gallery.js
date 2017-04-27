a0d5fad3ad4231ee43bd7ff885a26b63f9102b40
Unknown <agarcia318@mail.csuchico.edu>
2017-04-27 13:15:58 -0700

    Part 2 - Finished

--- gallery.js
+++ gallery.js
@@ -1,15 +1,52 @@
-var chico = ["Bear", "Horse", "Riley's", "Frankies", "TacoBell"];
+var chico = [
+
+  {description: "Crepuscular rays over the Saronic Gulf, Greece",
+
+   picture: "http://bing.yinsuso.com/cache/20170426.png" ,
+
+   from: "http://bing.yinsuso.com/"
+
+  },
+
+  {description: "Five Friends" ,
+
+   picture: "http://bing.yinsuso.com/cache/20170428.png" ,
+
+   from: "http://bing.yinsuso.com/?id=-1"
+
+  },
+
+  {description: "Morning Dew" ,
+
+   picture: "http://bing.yinsuso.com/cache/20170427.png" ,
+
+   from: "http://bing.yinsuso.com/"
+
+  },
+
+  {description: "Morning Clouds" ,
+
+   picture: "http://cdn.nanxiongnandi.com/bing/MiamiLights_EN-AU1356107223.jpg" ,
+
+   from: "http://bingwallpaper.anerg.com/au/201002"
+
+  }
+
+];
+
 var current;
 
 function shuffleGallery(){
 
-  var i = Math.floor(Math.random() * 5);
+  var i = Math.floor(Math.random() * chico.length);
 
   if(current == i){
     while (current == i){
-      var i = Math.floor(Math.random() * 5);
+      i = Math.floor(Math.random() * chico.length);
     }
   }
-  document.getElementById("demo").innerHTML = chico[i];
+  document.getElementById("caption").innerHTML = chico[i].description;
+  document.getElementById("picture").src = chico[i].picture;
+  document.getElementById("source").innerHTML = "Image courtesy of: " + chico[i].from;
   current = i;
 }
